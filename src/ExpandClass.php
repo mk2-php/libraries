4306a0191cc1bc7a5c704c3292059df6961fff21
nakatsuji <nktj_takakazu2014@yahoo.co.jp>
2021-01-17 15:44:54 +0900

    1/17

--- src/ExpandClass.php
+++ src/ExpandClass.php
@@ -4,12 +4,23 @@ namespace Mk2\Libraries;
 
 class ExpandClass{
 
+	private const CLASSTYPE_BACKPACK="Backpack";
+	private const CLASSTYPE_UI="UI";
+
     private $_classType;
-    private $_context;
+	private $_context;
+	private $extendNamespace;
     
     public function __construct($classType,&$context){
         $this->_classType=$classType;
-        $this->_context=$context;
+		$this->_context=$context;
+		
+		if($classType==self::CLASSTYPE_BACKPACK){
+			$this->extendNamespace="mk2\backpack_{className}\\";
+		}
+		else if($classType==self::CLASSTYPE_UI){
+			$this->extendNamespace="mk2\ui_{className}\\";
+		}
     }
 
     /**
@@ -34,17 +45,22 @@ class ExpandClass{
 			if(!empty($option["className"])){
 				$className=$option["className"];
 			}
-			
-            if(defined("MK2_DEFNS_".strtoupper($this->_classType))){
+
+			if(defined("MK2_DEFNS_".strtoupper($this->_classType))){
 				$namespace=constant("MK2_DEFNS_".strtoupper($this->_classType));
 			}
 			else{
 				$namespace=MK2_DEFNS."\\".$this->_classType;
 			}
+			$className2="\\".$namespace."\\".$className.$this->_classType;
 
-			$className="\\".$namespace."\\".$className.$this->_classType;
+			if(!class_exists($className2)){
+				if($this->extendNamespace){
+					$className2=str_replace("{className}",strtolower($className),$this->extendNamespace).$className.$this->_classType;
+				}
+			}
 
-			$classObject=new $className();
+			$classObject=new $className2();
 
 			$classObject->__parent=$this->_context;
 
